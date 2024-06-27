5784f0234dfb51b79c3f71816cc916531cbc8135
shalyne23 <shalynelangat@gmail.com>
2024-06-27 13:31:09 +0300

    review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+
 // Write your code in this file!
+const currentUser='andrew Nzioki'
+
+const welcomeMessage = `'Welcome to Flatbook, ' + ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const myString = 'template literal';
+
+const myNumber = 10;
+
+const myBoolean = false;
+
+`Saying that interpolation with ${myString}s is better than concatenation ${90 + myNumber}% of the time is simply ${myBoolean}. But it is pretty cool!
+
+Beware that new lines inside of a ${myString} will be preserved as new lines in the resulting ${typeof myString}!`;
+//=> "Saying that interpolation with template literals is better than concatenation 100% of the time is simply false. But it is pretty cool!
+
+// Beware that new lines inside of a template literal will be preserved as new lines in the resulting string!"
